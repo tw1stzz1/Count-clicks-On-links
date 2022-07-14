fb52828c2838a347c5c98a1e22ad066a7ef552fa
tw1stzz1 <egor4ig95@mail.ru>
2022-07-14 19:47:18 +0300

    Форматирование кода

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 def shorten_link(token, long_url):
     url = "https://api-ssl.bitly.com/v4/shorten"
     body = {"long_url": long_url}
-    header = {"Authorization" : "Bearer {}".format(token)}
+    header = {"Authorization": "Bearer {}".format(token)}
 
     response = requests.post(url, json=body, headers=header)
     response.raise_for_status()
@@ -19,13 +19,23 @@ def shorten_link(token, long_url):
 def count_clicks(token, bitlink):
     parsed_bitlink = urlparse(bitlink)
     url = "https://api-ssl.bitly.com/v4/bitlinks/{netloc}{path}/clicks/summary"
-    header = {"Authorization" : "Bearer {}".format(token)}
+    header = {"Authorization": "Bearer {}".format(token)}
 
-    response = requests.get(url.format(netloc=parsed_bitlink.netloc, path=parsed_bitlink.path), headers=header)
+    response = requests.get(url.format(
+        netloc=parsed_bitlink.netloc,
+        path=parsed_bitlink.path),
+        headers=header)
     response.raise_for_status()
 
     return response.json()["total_clicks"]
 
+
+def is_bitlink(url, token):
+    header = {"Authorization" : "Bearer {}".format(token)}
+    url = f"https://api-ssl.bitly.com/v4/bitlinks/{url}"
+    
+    response = requests.get(url, headers=header)
+    return response.ok
 
 if __name__ == "__main__":
     load_dotenv()
@@ -34,10 +44,10 @@ if __name__ == "__main__":
     url.add_argument('name', nargs='?')
 
     try:
-        try:
+        if is_bitlink(url, token): 
             clicks_count = count_clicks(token, url)
             print("Клики по ссылке = {}".format(clicks_count))
-        except requests.exceptions.HTTPError:
+        else:
             bitlink = shorten_link(token, url)
             print(bitlink)
     except requests.exceptions.HTTPError:
